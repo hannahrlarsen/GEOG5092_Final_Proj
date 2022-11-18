25747c67e0db3430440d01f0527165a18fc405c2
Casey Caduff <casey.caduff@gmail.com>
2022-11-17 18:08:15 -0700

    Created the file

---
+++ project.py
@@ -0,0 +1,28 @@
+import geopandas as gpd
+import pandas as pd
+
+path = 'C:/Users/larsenh/Downloads/'
+
+coord_list = []
+with open(path + 'Coordinates.txt') as file:
+    for coordinates in file:
+        temp_list = tuple(coordinates.strip().split('\t'))
+
+elements_list = []
+with open(path + 'Elements.txt') as file:
+    for ele in file:
+        temp_list = tuple(ele.strip().split('\t'))
+
+
+coords_df = pd.DataFrame(new_coords, columns=['Longitude', 'Latitude'])
+coords_gdf = gpd.GeoDataFrame(coords_df, geometry = gpd.points_from_xy(coords_df.Longitude, 
+            coords_df.Latitude))
+
+elements_df = pd.DataFrame(new_elements, columns=['Longitude', 'Latitude'])
+elements_gdf = gpd.GeoDataFrame(elements_df, geometry = gpd.points_from_xy(elements_df.Longitude, 
+            elements_df.Latitude))
+
+
+crs = {'init': 'epsg:4326'}
+coords_gdf = gpd.GeoDataFrame(coords_gdf, crs = crs)
+elements_gdf = gpd.GeoDataFrame(elements_gdf, crs = crs)
